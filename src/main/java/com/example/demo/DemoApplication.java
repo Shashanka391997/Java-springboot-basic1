d69d2086343f27c8328b3624804c8daa742f51a0
Shashanka12345 <shashanka.valere@gmail.com>
2024-06-23 11:58:50 +0530

    created basic student application in springboot

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import java.util.List;
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+ApplicationContext context= SpringApplication.run(DemoApplication.class, args);
+		Student s=context.getBean(Student.class);
+	s.setRollno(121);
+	s.setName("Ramesh");
+    s.setMarks(91);
+	StudentService service=context.getBean(StudentService.class);
+service.addStudent(s);
+List<Student> students=service.getStudents();
+System.out.println(students);
+}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student(rollno,name,marks) values(45,'Kiran',76);
+insert into student(rollno,name,marks) values(42,'Mirank',76);
+insert into student(rollno,name,marks) values(40,'Kiran',98);
